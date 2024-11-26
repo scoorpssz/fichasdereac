cef9f3d3ef0b3d1f3afba36a33b2b17cdfefa4de
José Cláudio <jvalentino9.2005@gmail.com>
2024-11-26 11:47:21 +0000

    abelhas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
+          src="/gym.svg"
+          alt="Gym Training logo"
           width={180}
           height={38}
           priority
