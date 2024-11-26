d4b676d82fdfca89573cc6cc70e523b534ad64fc
José Cláudio <jvalentino9.2005@gmail.com>
2024-11-26 11:21:06 +0000

    Content com as Imagens

--- src/app/components/Content.jsx
+++ src/app/components/Content.jsx
@@ -1,5 +1,31 @@
 export const Content = (props) => {
     <div>
     <Title>Top-3 Treinos favoritos</Title>
+
+    <div className="training">
+        {/* Treino  1 */}
+        <div className="train-box">
+        <img 
+            src="https://s3assets.skimble.com/assets/2289486/image_iphone.jpg"
+            alt="Bench Press"
+            />    
+        </div>
+
+        {/* Treino  2 */}
+        <div className="train-box">
+        <img 
+            src="https://lyfta.app/thumbnails/02391201.jpg"
+            alt="Straight Back Seated Row"
+            />    
+        </div>
+
+        {/* Treino  3 */}
+        <div className="train-box">
+        <img 
+            src="https://www.lyfta.app/_next/image?url=%2Fthumbnails%2F05771201.jpg&w=3840&q=20"
+            alt="Lever Chest Press"
+            />    
+        </div>
+    </div>
     </div>
     }
