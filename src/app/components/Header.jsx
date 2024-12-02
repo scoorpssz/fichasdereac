785162889fba777657d9ea5d037bb66747823c57
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-02 16:09:42 +0000

    blegh

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+//import React, {useState} from "react";
 export const Header = (props) => {
     return (
     <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 import { Header } from './components/Header';
 import { Content } from './components/Content';
 import { Footer } from './components/Footer';
-import React, {useState} from "react";
+//import React, {useState} from "react";
 
 export default function Home() {
   return (
