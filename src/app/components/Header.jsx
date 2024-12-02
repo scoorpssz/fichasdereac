c30ce14f01e5e5c466b590d21d15c6f85f34b4d8
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-02 16:01:09 +0000

    dasda

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,3 +1,4 @@
+import React, {useState} from "react";
 export const Header = (props) => {
     return (
     <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import styles from "./page.module.css";
 import { Header } from './components/Header';
 import { Content } from './components/Content';
 import { Footer } from './components/Footer';
+import React, {useState} from "react";
+
 export default function Home() {
   return (
     <div className={styles.page}>
