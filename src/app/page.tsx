3d17b90e6a91d9a58068b4fd1b21de6d1d9e8871
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-02 16:38:03 +0000

    sd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,20 @@ import styles from "./page.module.css";
 import { Header } from './components/Header';
 import { Content } from './components/Content';
 import { Footer } from './components/Footer';
-import React, {useState} from "react";
+//import React, {useState} from "react";
 
 
-function nameData() {
+/*function nameData() {
   const [my_name, SetName] = useState<String>()
   return<div>{my_name}</div>
-}
+} 
 
 function projectData() {
   const [project_name, SetProject] = useState<String>()
   
   return<div>{project_name}</div> 
-}
+}*/
+
 export default function Home() {
   return (
     <div className={styles.page}>
