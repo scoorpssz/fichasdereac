b95cdfbba64b19ee04f681f541cfd4d893bee43c
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-02 14:33:50 +0000

    meu deus

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,12 +1,8 @@
 export const Header = (props) => {
     return (
     <div>
-        <head>
             <h1>GYM TRAINING</h1>
-        </head>
-        <head>
-            <h3>Desenvolvido por José Davim</h3>
-        </head>
+            <h3>Desenvolvido por José Davim</h3>    
     </div>
 )
 }
