5a93aa73221e224aa7ca25173411b62b6654a55a
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-03 09:50:57 +0000

    correção

--- src/app/components/TrainInfo.jsx
+++ src/app/components/TrainInfo.jsx
@@ -3,12 +3,7 @@ import { Content } from './Content';
 export const TrainInfo = ({ props }) => {
     return (
         <div className="train-info">
-            <p>{props.description}</p>
-         <div className="train-box" key={props.id}>
-            <img src={props.img} alt={props.name} />
-            <p>{props.name}</p>
-         </div>
-        
+            <p>{props}</p>
         </div>
     );
 };
---
+++ src/app/shared/train.js
@@ -0,0 +1,22 @@
+const trainList = [
+    {
+        id: 1,
+            name: "Bench Press",
+            img: "https://s3assets.skimble.com/assets/2289486/image_iphone.jpg",
+            description: "Treino de força para o peitoral.",
+    },
+    {
+        id: 2,
+        name: "Straight Back Seated Row",
+        img: "https://lyfta.app/thumbnails/02391201.jpg",
+        description: "Exercício para fortalecer as costas.",
+    },
+    {
+        id: 3,
+            name: "Lever Chest Press",
+            img: "https://www.lyfta.app/_next/image?url=%2Fthumbnails%2F05771201.jpg&w=3840&q=20",
+            description: "Treino com foco em peito e tríceps.",
+    },
+]
+
+export default trainList
