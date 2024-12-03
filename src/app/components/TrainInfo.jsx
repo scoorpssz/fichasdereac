ae81aa0353ef7e04d8aa308e9cc285ae63ce39db
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-03 09:19:16 +0000

    blyat

--- src/app/components/TrainInfo.jsx
+++ src/app/components/TrainInfo.jsx
@@ -4,6 +4,9 @@ export const TrainInfo = ({ description }) => {
     return (
         <div className="train-info">
             <p>{description}</p>
+         className="train-box" key={train.id}
+        <img src={train.img} alt={train.name} />
+        <p>{train.name}</p>
         </div>
     );
 };
