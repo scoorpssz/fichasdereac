36f5121e61d5aad412207d6f26c9bf357058eee7
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-03 09:48:38 +0000

    d

--- src/app/components/TrainInfo.jsx
+++ src/app/components/TrainInfo.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Content } from './Content';
 export const TrainInfo = ({ props }) => {
     return (
         <div className="train-info">
-            <p>{props}</p>
+            <p>{props.description}</p>
+         <div className="train-box" key={props.id}>
+            <img src={props.img} alt={props.name} />
+            <p>{props.name}</p>
+         </div>
+        
         </div>
     );
 };
