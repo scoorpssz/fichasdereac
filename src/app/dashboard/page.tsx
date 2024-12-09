9124695e12a1e0c7e66be9e97c8d438a0e30abc8
José Cláudio <jvalentino9.2005@gmail.com>
2024-12-09 14:50:20 +0000

    amen finalmete está a dari yeyeyeyey

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,22 @@
 "use client";  
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Header } from "../components/Header";  
 import { Footer } from "../components/Footer";  
 import { TrainList } from "./components/trainlist";
-import './dashboard.css';  
+import './globals.css';  
 
 export default function Dashboard() {
-    const [my_name] = useState<String>("José Cláudio")
-    const [project_name] = useState<String>("Gym Training")
-  
+  const [my_name] = useState<String>("José Cláudio");
+  const [project_name] = useState<String>("Gym Training");
 
-    return (
-        <div className="dashboard-container">  
-            <div className="dashboard-content">  
-                <Header my_name={my_name} project_name={project_name}></Header>
-                <h1>Dashboard</h1>
-                <TrainList></TrainList>
-                <Footer my_name={my_name} project_name={project_name}></Footer>
-            </div>
-        </div>
-    );
+  return (
+    <div className="dashboard-container">  
+      <div className="dashboard-content">  
+        <Header my_name={my_name} project_name={project_name}></Header>
+        <h1>Dashboard</h1>
+        <TrainList></TrainList>
+        <Footer my_name={my_name} project_name={project_name}></Footer>
+      </div>
+    </div>
+  );
 }
